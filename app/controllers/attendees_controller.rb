df7ab0725e5540e7bbecd214b21af0f5a60e49d4
Mike Freeman <mfreema5@hawk.iit.edu>
2015-03-14 21:14:29 -0500

    Added grouping by trip to attendees controller and view. Also made changes to attendees.scss to make the table more comprehesible. Unfortunately, “rake test” now fails, because it's looking for a Trip.id that it can't find. Is it an issue with the fixtures?  Or do I need to be handling that in the controller somehow?

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -5,6 +5,9 @@ class AttendeesController < ApplicationController
   # GET /attendees.json
   def index
     @attendees = Attendee.order('trip_id')
+    @grouped_by_trips = @attendees.group_by { |attendee| attendee.trip_id } 
+      # See “Ramblings”, below
+    @trips = Trip.all
   end
 
   # GET /attendees/1
@@ -72,3 +75,41 @@ class AttendeesController < ApplicationController
       params.require(:attendee).permit(:supporter_id, :trip_id)
     end
 end
+
+
+# Ramblings
+
+# --> @attendees = Attendee.order('trip_id') <--
+# This makes a call on the database:  SELECT "attendees".* FROM "attendees"  ORDER BY trip_id
+# What gets returned is an array of hashes.
+# Each hash is one row from the table.
+# The keys are the column names, the values are the column values.
+# The hashes are ordered in the array by the column "trip_id".
+# When you have ‘attendees.each’, you're iterating through the array.
+
+# --> @grouped_by_trips = @attendees.group_by { |attendee| attendee.trip_id } <--
+# This operates on the array that contains hashes that are table rows.
+# It returns a hash of hashes.
+# For the outer hash:
+#   the keys are the values of ‘attendee.trip_id’
+#   the values are all the table rows with ‘attendee.trip_id’ that matches the key
+# For the nested hash:
+#   the keys are the column names of the table
+#   the values are the column values of the table
+
+# Compressed/formatted example:
+#
+# > @grouped_by_ts = @as.group_by { |a| a.t_id }
+# => {1=>[#<A id: 1, s_id: 1, t_id: 1>, 
+#         #<A id: 2, s_id: 4, t_id: 1>, 
+#         #<A id: 6, s_id: 5, t_id: 1>], 
+#     3=>[#<A id: 3, s_id: 2, t_id: 3>, 
+#         #<A id: 4, s_id: 3, t_id: 3>, 
+#         #<A id: 5, s_id: 1, t_id: 3>]} 
+
+# In the view, you iterate through the hash, with another nested iteration to go
+# through the nested hash.  Something like:
+#
+# 
+
+
